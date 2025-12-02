cd487b85c764c651b29048bfda3619763d2afb68
akhil <akhil.d@digitelenetworks.com>
2025-12-02 23:43:47 +0530

    added new changes

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -10,8 +10,8 @@ import { NgFor, NgIf } from '@angular/common';
 })
 export class ContactComponent {
   readonly contacts = [
-    { label: 'Phone', value: '+1 202 555 789', href: 'tel:+1202555789' },
+    { label: 'Phone', value: '+1 905 278 8898', href: 'tel:+1905278998' },
     { label: 'Email', value: 'order@wingshouse.com', href: 'mailto:order@wingshouse.com' },
-    { label: 'Location', value: '130 Downtown Street, Boston, MA 02116' }
+    { label: 'Location', value: '20497 Leslie St Queensville, Ontario, Canada, Postal Code L0G 1R0' }
   ];
 }
